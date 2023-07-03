fbb6371602239dfaa54e3bdd67cf2cad1fc185a5
Dinis <Dinis_abd@example.com>
2023-07-03 18:59:08 +0300

    add hw6

---
+++ HW_6/main_task2.3.py
@@ -0,0 +1,9 @@
+
+
+from chess.queen import generator_position, check_queen
+
+if __name__ == '__main__':
+    print(check_queen([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8]]))
+    generator_position(4)
+
+
